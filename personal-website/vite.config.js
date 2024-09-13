537f7d8fbd94e9511f06ddbdbad8132bc8ff7e5f
bsule <bilal.suleiman@gmail.com>
2024-09-13 16:54:46 -0700

    deployment changes

--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bsule.github.io",
+  base: "/bsule.github.io/personal-website",
   plugins: [react()],
 })
